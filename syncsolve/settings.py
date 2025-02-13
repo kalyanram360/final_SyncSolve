f48ce6d90b32836084f2ec5189ac0585be8d5cf1
kalyan <mkalyan8400@gmail.com>
2025-02-14 00:19:13 +0530

    settings chd

--- syncsolve/settings.py
+++ syncsolve/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tb-3r3ln4dvli$&lko%doqcou2#@h$!ri9%&r8u42uq2jdy)ks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://final_syncsolve.up.railway.app/']
+ALLOWED_HOSTS = ['final_syncsolve.up.railway.app']
 
 
 # Application definition
