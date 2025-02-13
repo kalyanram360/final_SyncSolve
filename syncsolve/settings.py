4808796653150ade0a1314d5438fb2479fd8c45d
kalyan <mkalyan8400@gmail.com>
2025-02-13 23:19:18 +0530

    debug false

--- syncsolve/settings.py
+++ syncsolve/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tb-3r3ln4dvli$&lko%doqcou2#@h$!ri9%&r8u42uq2jdy)ks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
