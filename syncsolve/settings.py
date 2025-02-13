042a6296907e8d82f3289394f83ea7870765b9cb
kalyan <mkalyan8400@gmail.com>
2025-02-13 23:13:07 +0530

    allowed hosts

--- syncsolve/settings.py
+++ syncsolve/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-tb-3r3ln4dvli$&lko%doqcou2#@h$!ri9%&r8u42uq2jdy)ks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
